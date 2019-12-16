d4159a47c023a0b6a5070e602c39fffc597cd323
nirrvana <wlwhs91@gmail.com>
2019-12-17 00:53:41 +0900

    Nav 컴포넌트 구현

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import TodoListContainer from './ToDoListContainer';
+import React from "react";
+import TodoListContainer from "./ToDoListContainer";
 
-const Nav = () => (
-  <TodoListContainer />
-)
+const Nav = (props) => (
+  <div className="nav">
+    <TodoListContainer 
+      allTodoList={props.allTodoList}
+      selectTodoList={props.selectTodoList}
+    />    
+    <button className="addTodoListBtn" onClick="">목록추가</button>
+  </div>
+);
 
 export default Nav;
