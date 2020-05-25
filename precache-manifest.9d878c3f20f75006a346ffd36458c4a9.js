0a18e12fb69b1fde456d8bfcf0f8f49e6a61594a
nirrvana <wlwhs91@gmail.com>
2020-05-25 16:02:21 +0900

    Updates

---
+++ precache-manifest.9d878c3f20f75006a346ffd36458c4a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10464d19443fd2575b2e435cbe311d0d",
+    "url": "/todo-practice/index.html"
+  },
+  {
+    "revision": "2a18390659273ef5ab6f",
+    "url": "/todo-practice/static/js/2.855ee4c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-practice/static/js/2.855ee4c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "637bb462ccbf4abb1d60",
+    "url": "/todo-practice/static/js/main.a3eb027d.chunk.js"
+  },
+  {
+    "revision": "09f501b374a3f8ca9596",
+    "url": "/todo-practice/static/js/runtime-main.81fbbeb0.js"
+  }
+]);
